00cdc4bcdc82cbbaa5c4d4e82c31d3190f6abb56
Mohammad Hassan <62506881+mohammadhy@users.noreply.github.com>
2023-12-21 23:15:22 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
 echo "welcome" > /home/user/test
+echo $(ls)
 for manifest in $(ls)
 do
   if [[ $manifest == *.yaml ]]
   then 	  
       echo $manifest
-      kubectl apply -f $manifest
+      echo "123" | sudo -s kubectl apply -f $manifest
   fi
 
 done
