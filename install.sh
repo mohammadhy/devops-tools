456f0c309733e51e0745b800086810b3e7ad4c4b
Mohammad Hassan <62506881+mohammadhy@users.noreply.github.com>
2023-12-21 23:52:25 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ echo $m >> /home/user/example
   if [[ $m == *.yaml ]]
   then 	  
       echo $m >> /home/user/test
-      kubectl apply -f $m
-      echo "done:" $m 
+      echo `kubectl apply -f $m`
+      #echo "done:" $m 
   fi
 done
