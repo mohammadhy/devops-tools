23966fa47405ff6f10df74f23c1c7f886ed22491
Mohammad Hassan <62506881+mohammadhy@users.noreply.github.com>
2023-12-22 13:57:12 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,12 @@
 #! /bin/bash
-#echo "welcome" > /home/user/test
-#echo $(ls manifest/)
 cd manifest
-echo `pwd`
 for m in $(ls)
 do
   echo $m
   if [[ $m == *.yaml ]]
   then
-      echo "/home/user/manifest/"$m >> /home/user/test
-      path=$(echo `pwd`$m)
+      maindir=$(echo `pwd`)
+      path=echo $maindir/$m
       kubectl apply -f $path
   fi
 done
