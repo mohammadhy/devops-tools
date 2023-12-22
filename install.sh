4d8bdc005bcb71a2e9605d08ef34fe87c43f5743
Mohammad Hassan <62506881+mohammadhy@users.noreply.github.com>
2023-12-22 16:14:53 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,13 +4,13 @@ echo "Install Keyverno For Policy"
 kubectl create -f https://github.com/kyverno/kyverno/releases/download/v1.10.0/install.yaml
 for m in $(ls)
 do
-  echo $m
+  #echo $m
   if [[ $m == *.yaml ]]
   then
       maindir=$(echo `pwd`)
-      echo $maindir
+      #echo $maindir
       path=$(echo $maindir/$m)
-      echo $path
+      #echo $path
       kubectl apply -f $path
   fi
 done
